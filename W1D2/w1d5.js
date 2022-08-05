1a754872e95c9adad7df03b492f611abd5eb8351
joeytsui1 <joeytsui131@gmail.com>
2022-08-05 18:18:54 -0400

    'day4ps'

---
+++ W1D2/w1d5.js
@@ -0,0 +1,120 @@
+function isInside(array, ele) { 
+
+    if (array.indexOf(ele) > -1 ) {
+        return true
+    } else { 
+        return false
+    }
+}
+
+console.log(isInside(['chase', 'phi', 'elliot'], 'elliot'))
+
+function printReverse(str) {
+let reverseWord = ''
+
+for (i = str.length -1; i >= 0; i-- ) {
+    let  char = str[i]
+    reverseWord += char
+}
+
+return reverseWord
+}
+
+console.log(printReverse('bootcamp'))
+
+// Write a function `luckySevens(max)` that returns an array of all numbers up
+// to max (inclusive) that are divisible by 7.
+//
+// Examples:
+//
+// luckySevens(25); // => [ 7, 14, 21 ]
+// luckySevens(42); // => [ 7, 14, 21, 28, 35, 42 ]
+
+function luckySevens(max) {
+let arr4 = []
+
+for (i = 1; i <= max; i++) {
+    if ( i % 7 === 0 ) {
+        arr4.push(i)
+    } 
+}
+return arr4
+}
+
+console.log(luckySevens(25))
+console.log(luckySevens(42))
+
+// Write a function `copyMachine(element, num)` that takes in an element and a number
+// it should return an array of length `num` that is filled with `element`.
+//
+// Examples:
+//
+// copyMachine('candy', 0); // => []
+// copyMachine('candy', 2); // => [ 'candy', 'candy' ]
+// copyMachine('bread', 4); // => [ 'bread', 'bread', 'bread', 'bread' ]
+// copyMachine(11, 6); // => [ 11, 11, 11, 11, 11, 11 ]
+
+function copyMachine (element, num) {
+
+}
+
+// Write a function `everyOtherWord(sentence)` that takes in a sentence and returns
+// an array containing every other word in that sentence.
+//
+// Examples:
+//
+// everyOtherWord('hello how are you doing on this lovely day?'); // => [ 'hello', 'are', 'doing', 'this', 'day?' ]
+// everyOtherWord('the weather is wonderful'); // => [ 'the', 'is' ]
+
+function everyOtherWord(sentence) {
+
+}
+
+// Write a function `wordYeller(sentence)` that takes in a sentence string as
+// input. It should return the sentence where every word has an exclamation
+// point behind it, except words that already have punctuation. Punctuation
+// marks are ". , ! ? ; :"
+//
+// Example 1
+// var sent = "Stop it now! Please, wont you stop?";
+// var yelledWords = wordYeller(sent);
+// yelledWords === "Stop! it! now! Please, wont! you! stop?"; // => true
+//
+// Example 2
+// var words = "Go to the store and grab the following: milk, bread, run, and cake";
+// wordYeller(words) === "Go! to! the! store! and! grab! the! following: milk, bread, run, and! cake!"; // => true
+
+function wordYeller(sentence) {
+
+}
+
+// Write a function `arraySubstring(words, str)` that takes in an array of
+// strings and a string as arguments and returns an array where each element is
+// replaced with true if the string is a substring of the element and false if
+// it is not.
+//
+// Examples:
+//
+// arraySubstring(["hello", "history", "helix", "hellos"],"hel")
+// => [true, false, true, true]
+// arraySubstring(["prefix", "problems", "pragmatic", "prefer"], "pre")
+// => [true, false, false, true]
+
+function arraySubstring(words, str) { 
+
+}
+
+// Write a function evenCaps(sentence) that takes in a sentence as an
+// argument and returns the same string with each even index being
+// capitalized. Index 0 counts as even.
+//
+// Examples:
+//
+// evenCaps("Tom got a small piece of pie")
+// => "ToM GoT A SmAlL PiEcE Of pIe"
+// evenCaps("the book is in front of the table")
+// => "ThE BoOk iS In fRoNt oF ThE TaBlE"
+
+function evenCaps (sentence) {
+
+}
